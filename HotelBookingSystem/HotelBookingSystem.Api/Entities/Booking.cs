86c99c7c46099f3c4e51932c150ee1a1460be0e7
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-03-12 21:00:31 +0400

    Add User, Hotel, Room and Booking entities

---
+++ HotelBookingSystem/HotelBookingSystem.Api/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelBookingSystem.Api.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public int RoomId { get; set; }
+        public Room Room { get; set; } = null!;
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly CheckOutDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
