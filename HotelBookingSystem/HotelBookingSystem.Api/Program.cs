ea85686450bfe6b5f9cd5424da4456c2447312f4
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-03-12 21:52:06 +0400

    Add database setup and initial migration

--- HotelBookingSystem/HotelBookingSystem.Api/Program.cs
+++ HotelBookingSystem/HotelBookingSystem.Api/Program.cs
@@ -1,4 +1,9 @@
+using HotelBookingSystem.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
