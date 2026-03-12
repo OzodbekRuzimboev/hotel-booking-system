e35ea05b603623105c3836cee1b7d9b266bb280e
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-03-12 22:10:57 +0400

    Implement hotel list and details endpoints

---
+++ HotelBookingSystem/HotelBookingSystem.Api/Contracts/RoomResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingSystem.Api.Contracts
+{
+    public class RoomResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
