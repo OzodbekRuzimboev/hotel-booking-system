0b8476a5e2a66ecf29e98b1b585ddd25fa2efc39
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-03-12 22:10:57 +0400

    Implement hotel list and details endpoints

---
+++ HotelBookingSystem/HotelBookingSystem.Api/Contracts/HotelResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingSystem.Api.Contracts
+{
+    public class HotelResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
---
+++ HotelBookingSystem/HotelBookingSystem.Api/Controllers/HotelsController.cs
@@ -0,0 +1,59 @@
+﻿using HotelBookingSystem.Api.Contracts;
+using HotelBookingSystem.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HotelsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public HotelsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetHotels()
+        {
+            var hotels = _context.Hotels.Select(h => new HotelResponse
+            {
+                Id = h.Id,
+                Name = h.Name,
+                City = h.City,
+                Address = h.Address
+            }).ToList();
+
+            return Ok(hotels);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetHotelById(int id)
+        {
+            var hotel = _context.Hotels.Include(h => h.Rooms).SingleOrDefault(h => h.Id == id);
+
+            if (hotel == null)
+                return NotFound();
+
+            var response = new HotelDetailsResponse
+            {
+                Id = hotel.Id,
+                Name = hotel.Name,
+                City = hotel.City,
+                Address = hotel.Address,
+                Rooms = hotel.Rooms.Select(r => new RoomResponse
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    Capacity = r.Capacity,
+                    Price = r.Price
+                }).ToList()
+            };
+
+            return Ok(response);
+        }
+    }
+}
