e0012746728b944706e46209f11d3856fc50fe5c
Ozodbek <ozodbekruzimboev5@gmail.com>
2026-03-26 16:49:05 +0400

    Add integration test infrastructure with postgres container

---
+++ HotelBookingSystem/HotelBookingSystem.Api.IntegrationTests/ApiFactory.cs
@@ -0,0 +1,58 @@
+﻿using HotelBookingSystem.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
+
+namespace HotelBookingSystem.Api.IntegrationTests
+{
+    public sealed class ApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _db = new PostgreSqlBuilder("postgres:16")
+            .WithDatabase("hotel_booking_tests")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+        public async Task InitializeAsync()
+        {
+            await _db.StartAsync();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development");
+
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql(_db.GetConnectionString()));
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                db.Database.Migrate();
+            });
+        }
+
+        public async Task ResetDatabaseAsync()
+        {
+            using var scope = Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.MigrateAsync();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _db.DisposeAsync();
+        }
+    }
+}
